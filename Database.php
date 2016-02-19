445965bea3404fdc6efc65d85619c8a8f381994d
loquox <crloquox@gmail.com>
2016-02-19 19:27:52 +0100

    clase Meta

--- Database.php
+++ Database.php
@@ -37,7 +37,7 @@ class  Database {
                 USERNAME,
                 PASSWORD,
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_ECEPTION);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         }
         
---
+++ Meta.php
@@ -0,0 +1,28 @@
+<?php 
+
+//Representa la estructura de las metas
+require 'Database.php';
+
+class Meta
+{
+    function __construct(){
+        
+    }
+    
+    public static function getAll(){
+        $consulta = "SELECT * FROM meta";
+        try{
+            //Preparar sentencia
+            $comando = Database::getInstance()->getDb()->prepare($consulta);
+            //Ejecutar sentencia preparada
+            $comando->execute();
+            
+            return $comando->fetchAll(PDO::FETCH_ASSOC);
+                       
+        }catch (PDOException $e){
+            return false;
+        }
+    }
+     
+    
+}
