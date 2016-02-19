b056663de7c7fb116a6c885000ee6954b81441a3
loquox <crloquox@gmail.com>
2016-02-19 20:23:34 +0100

    script para obtener todas las metas

---
+++ obtener_metas.php
@@ -0,0 +1,22 @@
+<?php 
+//Obtiene todas las metas de la base de datos
+
+require 'Meta.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    
+    //Manejar petición GET
+    $metas = Meta::getAll();
+    if ($metas) {
+        $datos["estado"] = 1;
+        $datos["metas"] = $metas;
+        
+        print json_encode(Sdatos);
+    } else {
+        print json_encode(array(
+        "estado" => 2,
+        "mensaje" => "Ha ocurrido un error"
+        ));
+        
+    }
+}
