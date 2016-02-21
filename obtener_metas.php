a08c76d6edcec5b3271d825e78bf763b7fdec56a
loquox <crloquox@gmail.com>
2016-02-21 18:01:17 +0100

    otra vez

--- obtener_metas.php
+++ obtener_metas.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 //Obtiene todas las metas de la base de datos
 
 require 'Meta.php';
 
 //if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    
-    //Manejar petición GET
+
+    //Manejar petición GET ALL para obtener metas
     $metas = Meta::getAll();
     if ($metas) {
         $datos["estado"] = 1;
         $datos["metas"] = $metas;
-        
+
         print json_encode($datos);
     } else {
         print json_encode(array(
         "estado" => 2,
         "mensaje" => "Ha ocurrido un error"
         ));
-        
+
     }
 //}
 
