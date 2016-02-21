b39a3b5bfd103fecccf643c8b89d0f5cdc4092aa
loquox <crloquox@gmail.com>
2016-02-21 10:29:23 +0100

    commit con atom

--- Database.php
+++ Database.php
@@ -3,57 +3,61 @@
 
 require_once 'mysql_login.php';
 
-class  Database { 
-    
+class  Database {
+
     //Única instancia de la clase
     private static $db = null;
-    
+
     //Instancia de PDO
     private static $pdo;
-    
+
+
+
     final private function __construct(){
         try{
-            //Crear nueva conexió PDO
+            //Crear nueva conexión PDO
             self::getDb();
+
+
         }catch(PDOException $e){
             //Manejo de excepciones
         }
     }
-    
+
     public static function getInstance(){
-        
+
         if (self::$db === null){
             self::$db = new self();
         }
         return self::$db;
     }
-    
+
     public function getDb(){
         if (self::$pdo == null){
             self::$pdo = new PDO(
                 'mysql:dbname='. DATABASE .
-                ';host=' . HOSTNAME .   
+                ';host=' . HOSTNAME .
                 ';port:63343;' ,
                 USERNAME,
                 PASSWORD,
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
         }
-        
+
         return self::$pdo;
     }
-    
+
     final protected function __clone()
     {
-        
+
     }
-    
+
     function _destructor(){
         self::$pdp = null;
     }
-    
-    
+
+
 }
 
 ?>
