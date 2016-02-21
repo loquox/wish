7e18ea7f213418fe0261169eb37d0372605fb070
loquox <crloquox@gmail.com>
2016-02-21 17:31:55 +0100

    algunos cambios

--- Meta.php
+++ Meta.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //Representa la estructura de las metas
 require 'Database.php';
@@ -6,25 +6,25 @@ require 'Database.php';
 class Meta
 {
     function __construct(){
-        
+
     }
-    
+
     public static function getAll(){
         $consulta = "SELECT * FROM meta";
         try{
             //Preparar sentencia
             $comando = Database::getInstance()->getDb()->prepare($consulta);
-            //Ejecutar sentencia preparada
+            //Ejecutar sentencia preparada total
             $comando->execute();
-            
+
             return $comando->fetchAll(PDO::FETCH_ASSOC);
-                       
+
         }catch (PDOException $e){
             echo 'Fallo la conexion :' . $e->getMessage();
-            
+
             return false;
         }
     }
-     
-    
+
+
 }
